50ff3e754e8364c5dce4cef8dea2217cbb8ad5d0
flintman <flintman@flintmancomputers.com>
2014-11-28 09:16:09 -0500

    gpu:  fix mismerge
    Change-Id: Ia80e80a72e8da0991267c099c3e18c782467b42c

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -914,9 +914,6 @@ static void kgsl_destroy_process_private(struct kref *kref)
 	kgsl_mmu_putpagetable(private->pagetable);
 	idr_destroy(&private->mem_idr);
 
-	list_del(&private->list);
-	mutex_unlock(&kgsl_driver.process_mutex);
-
 	kfree(private);
 	return;
 }
